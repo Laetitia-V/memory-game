333c77ab1ded7a029b7bce4309dd9a88a79872dd
Laetitia-V <laetitia.viau@hotmail.fr>
2022-03-17 16:09:57 +0100

    Create JS script file

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$( function() {
+     $( "#slider-level" ).slider({
+          min:1,
+          max:5,
+          value:2
+     });
+} );
+
+$( function() {
+     $( "#progress-bar" ).progressbar();
+} );
