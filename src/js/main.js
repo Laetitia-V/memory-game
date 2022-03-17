a8f22c3a7bffb967c225f3b4d8da8f0b72299171
Laetitia-V <laetitia.viau@hotmail.fr>
2022-03-17 16:35:48 +0100

    feat: load cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,42 @@
 $( function() {
+     addSlider();
+     addProgressBar();
+     loadCards();
+});
+
+function addSlider() {
      $( "#slider-level" ).slider({
           min:1,
           max:5,
           value:2
      });
-} );
+} 
 
-$( function() {
+function addProgressBar() {
      $( "#progress-bar" ).progressbar();
-} );
+}
+
+function loadCards() {
+     $.getJSON("../../public/cards/cards.json", function(cards){
+          for(const card of cards){
+               addCard(card);
+          }
+      }).fail(function(){
+          console.log("An error has occurred.");
+      });
+}
+
+function addCard(card) {
+     const name = card.name;
+     const file = "../../public/cards/" + name + ".png";
+
+     for (i of [0, 1]){
+          const id = name + i;
+          $( "#board-game").append($("<img></img>")
+                         .attr({
+                              src: file,
+                              id: id,
+                              class: "board-game__card" 
+                         }));
+     }
+}
