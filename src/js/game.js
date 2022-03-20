5ff31e5cb047f41b2b7a09d49c75684bbd763bab
Laetitia-V <laetitia.viau@hotmail.fr>
2022-03-20 16:27:21 +0100

    refactor(start game): create functions

--- src/js/game.js
+++ src/js/game.js
@@ -42,3 +42,45 @@ function loadCards() {
           console.log("An error has occurred.");
      });
 }
+
+function startGame() {
+     
+     setProgressBar();
+
+     let i = 1; 
+     stopwatch = setInterval(function () {       
+          const minutes = (Math.floor(i / 60)).toString();
+          const seconds = (i % 60).toString();
+          const result = `${minutes.padStart(2, '0')}:${seconds.padStart(2, '0')}`;
+          $("#time").html(result);         
+          i++;
+     }, 1000);
+
+     $( "#game > .board-game > div").on("click", function(){
+          const img = this.firstChild;
+          $(img).css("display", "block");
+          if (Object.keys(cardSelected).length == 0) {
+               cardSelected[1] = img;
+          }
+          else if (Object.keys(cardSelected).length == 1) {
+               cardSelected[2] = img;
+          }
+          else if (Object.keys(cardSelected).length == 2) {
+               const firstCardName = $(cardSelected[1]).attr("class");
+               const secondCardName = $(cardSelected[2]).attr("class");
+               if (firstCardName != secondCardName) {
+                    $(cardSelected[1]).css("display", "none");
+                    $(cardSelected[2]).css("display", "none");
+               }
+               else {
+                    nbCardPairsFound += 1;
+                    updateProgressBar();
+                    if (nbCardPairsFound == nbCardPairsToFound) clearInterval(stopwatch);
+                    $(cardSelected[1]).parent().css("opacity", "0");
+                    $(cardSelected[2]).parent().css("opacity", "0");
+               }
+               cardSelected = {};
+               cardSelected[1] = img;
+          }
+     });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,57 +2,16 @@ let progressBar;
 let stopwatch;
 
 $( function() {
-     addSlider();
+     //addSlider();
      loadCards();
      addProgressBar();
      addStopwatch();
 });
 
 function addStopwatch() {
-     $("#start").click(function (e) {
-          progressBar.progressbar({
-               max: nbCardPairsToFound,
-               value: nbCardPairsFound
-          });
-          $( "#game > .board-game > div").click(function() {
-               const img = this.firstChild;
-               $(img).css("display", "block");
-               if (Object.keys(cardSelected).length == 0) {
-                    cardSelected[1] = img;
-               }
-               else if (Object.keys(cardSelected).length == 1) {
-                    cardSelected[2] = img;
-               }
-               else if (Object.keys(cardSelected).length == 2) {
-                    const firstCardName = $(cardSelected[1]).attr("class");
-                    const secondCardName = $(cardSelected[2]).attr("class");
-                    if (firstCardName != secondCardName) {
-                         $(cardSelected[1]).css("display", "none");
-                         $(cardSelected[2]).css("display", "none");
-                    }
-                    else {
-                         nbCardPairsFound += 1;
-                         updateProgressBar();
-                         if (nbCardPairsFound == nbCardPairsToFound) clearInterval(stopwatch);
-                         $(cardSelected[1]).parent().css("opacity", "0");
-                         $(cardSelected[2]).parent().css("opacity", "0");
-                    }
-                    cardSelected = {};
-                    cardSelected[1] = img;
-               }
-          })
-          stopwatch = setInterval(function () {       
-               const minutes = Math.floor(i / 60);
-               const seconds = i % 60;
-
-               function padTo2Digits(num) {
-                    return num.toString().padStart(2, '0');
-               }
-               const result = `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`;
-               $("#time").html(result);         
-               i++;
-          }, 1000);
-      });
+     $("#start").on("click", function (e) {
+          startGame();
+     });
 }
 
 function addSlider() {
@@ -67,9 +26,14 @@ function addProgressBar() {
      progressBar = $( "#progress-bar" ).progressbar();
 }
 
+function setProgressBar() {
+     progressBar.progressbar({
+          max: nbCardPairsToFound
+     });
+}
+
 function updateProgressBar() {
      progressBar.progressbar({
-          //max: nbCardPairsToFound,
           value: nbCardPairsFound
      });
 }
